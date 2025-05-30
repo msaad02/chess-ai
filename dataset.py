9236b28d7ce508212f957921d6843283ffa3009d
Matthew Saad <msaad02@outlook.com>
2025-05-30 11:42:32 -0400

    make iterable dataset, investigate dataloader issue

--- dataset.py
+++ dataset.py
@@ -1,110 +1,66 @@
+from torch.utils.data import IterableDataset, get_worker_info
 from pathlib import Path
 import numpy as np
 import torch
 import json
+import random
 
 
-class LichessDataset(torch.utils.data.Dataset):
-    """
-    Serves up data according to the batch, inherits pytorch Dataset
-    which allows better parallelism and integration with pytorch
-    """
+class LichessStream(IterableDataset):
+    def __init__(self, batches: list[Path], shuffle_batches: bool = False):
 
-    def __init__(
-        self,
-        root_dir: str | Path,
-        minmax_batch: tuple[int, int] = (0, float("inf")),
-        device: torch.device = None,
-    ):
-        """Initializes dataset and loads the first batch"""
+        self.shuffle_batches = shuffle_batches
+        self.batches = batches
 
-        self.device = device
-
-        self.batches = sorted(
-            Path(root_dir).iterdir(), key=lambda p: int(p.name.split("_")[1])
-        )
-
-        batch_list = [
-            p
-            for p in self.batches
-            if minmax_batch[0] <= int(p.name.split("_")[1]) < minmax_batch[1]
-        ]
-
-        self.batches = {i: p for i, p in enumerate(batch_list)}
-
-        self.x_files = {
-            id: Path(batch, "board_vecs.npz") for id, batch in self.batches.items()
-        }
-        self.y_files = {
-            id: Path(batch, "next_moves.json") for id, batch in self.batches.items()
-        }
-
-        self.batch_ranges = {}
         distinct_moves = set()
-        batch_start = 0
-        for batch_id, batch_targets_file in self.y_files.items():
-            with open(batch_targets_file) as f:
+        self._length = 0
+        for batch in self.batches:
+            with open(batch / "next_moves.json") as f:
                 batch_targets = json.load(f)
-
-                self.batch_ranges[batch_id] = {
-                    "min": batch_start,
-                    "max": batch_start + len(batch_targets),
-                }
                 distinct_moves.update(batch_targets)
-
-                batch_start += len(batch_targets)
+                self._length += len(batch_targets)
 
         self.idx_to_move = {
             idx: move for idx, move in enumerate(sorted(distinct_moves))
         }
         self.move_to_idx = {move: idx for idx, move in self.idx_to_move.items()}
 
-        self._length = batch_start
-
-        self.curr_batch_id = 0
-        self.curr_batch = self.load_new_batch(list(self.batches.keys())[0])
-
-    def load_new_batch(
-        self, batch_id: int
-    ) -> dict[str : torch.Tensor, str : torch.Tensor]:
-        """Load all X/Y values in batch"""
-
-        metadata = np.load(self.x_files[batch_id], mmap_mode="r")
-        shape, data = metadata["shape"], metadata["data"]
-        bits = np.unpackbits(data, bitorder="little")[: np.prod(shape)]
-        x = torch.from_numpy(bits.reshape(shape).astype(np.float32))
-
-        # Load Y with new batch
-        with open(self.y_files[batch_id]) as f:
-            y = torch.tensor(
-                [self.move_to_idx.get(x) for x in json.load(f)], dtype=torch.int16
-            )
-
-        if self.device:
-            x = x.to(self.device)
-            y = y.to(self.device)
-
-        return {"x": x, "y": y}
-
     def __len__(self):
         return self._length
 
-    def __getitem__(self, idx):
+    def _yield_rows_from_batch(self, batch_dir):
+        metadata = np.load(batch_dir / "board_vecs.npz", mmap_mode="r")
+        shape, data = metadata["shape"], metadata["data"]
+        bits_per_row = int(np.prod(shape[1:]))
+        with open(batch_dir / "next_moves.json") as f:
+            y_list = [self.move_to_idx[m] for m in json.load(f)]
 
-        batch_info = {
-            id: range
-            for id, range in self.batch_ranges.items()
-            if range["min"] <= idx < range["max"]
-        }
+        for row in range(shape[0]):
+            byte0 = (row * bits_per_row) // 8
+            byte1 = (row * bits_per_row + bits_per_row + 7) // 8
+            bits = np.unpackbits(data[byte0:byte1], bitorder="little")[:bits_per_row]
+            yield bits, y_list[row]
 
-        assert batch_info, IndexError
+    def __iter__(self):
+        info = get_worker_info()
+        if info is None:
+            my_batches = self.batches
+            rng = random.Random(42)
+        else:
+            num_w, wid = info.num_workers, info.id
+            my_batches = self.batches[wid::num_w]
+            rng = random.Random(42 + wid)
 
-        batch_id = list(batch_info.keys())[0]
-        if batch_id != self.curr_batch_id:
+        while True:
+            if self.shuffle_batches:
+                rng.shuffle(my_batches)
+            for b in my_batches:
+                yield from self._yield_rows_from_batch(b)
 
-            self.curr_batch = self.load_new_batch(batch_id)
-            self.curr_batch_id = batch_id
 
-        idx_real = idx - batch_info[batch_id]["min"]
-
-        return self.curr_batch["x"][idx_real], self.curr_batch["y"][idx_real]
+def collate(batch):
+    xs, ys = zip(*batch)
+    # Casting here instead of LichessStream to save memory
+    xs = torch.tensor(np.stack(xs), dtype=torch.float32)
+    ys = torch.tensor(ys, dtype=torch.long)
+    return xs, ys
--- model.py
+++ model.py
@@ -1,53 +1,75 @@
-from torch.utils.data import DataLoader, random_split
-from dataset import LichessDataset
+from torch.utils.data import DataLoader
+from dataset import LichessStream, collate
+from pathlib import Path
 import torch
 import wandb
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-batch_size = 2048
-num_epochs = 256
+batch_size = 75000
+num_epochs = 8
 
 learning_rate = 0.0001
 
 # ----- Dataset ----------------------------------
 
-dataset = LichessDataset("data/split_data")
+# Get the batches of preprocessed data
+batches = list(Path("data/split_data").glob("batch_*"))
 
-train_data, test_data = random_split(dataset, [0.8, 0.2])
+# Create train/test split
+split = int(0.8 * len(batches))
 
-train_loader = DataLoader(train_data, batch_size=batch_size)
-test_loader = DataLoader(test_data, batch_size=batch_size)
+# train_stream = LichessStream(batches=batches[:split])
+# valid_stream = LichessStream(batches=batches[split:])
 
-idx_to_move = dataset.move_to_idx
+train_stream = LichessStream(batches=batches[:1])
+valid_stream = LichessStream(batches=batches[1:2])
+
+train_loader = DataLoader(
+    train_stream,
+    batch_size=batch_size,
+    # num_workers=3,
+    collate_fn=collate,
+    # prefetch_factor=4,
+    pin_memory=True,
+    drop_last=True,
+)
+
+valid_loader = DataLoader(
+    valid_stream,
+    batch_size=batch_size,
+    # num_workers=1,
+    collate_fn=collate,
+    # prefetch_factor=4,
+    pin_memory=True,
+    drop_last=True,
+)
+
+
+# Need to come up with a unified "vocab"
+idx_to_move = train_stream.idx_to_move
 
 
 # ----- Model -----------------------------------
 class ImitationModel(torch.nn.Module):
     def __init__(self, in_features, out_features):
         super(ImitationModel, self).__init__()
-        self.linear1 = torch.nn.Linear(in_features, 20_000)
-        self.linear2 = torch.nn.Linear(20_000, 10_000)
-        self.linear3 = torch.nn.Linear(10_000, 5_000)
-        self.linear4 = torch.nn.Linear(5_000, out_features)
-        self.activation = torch.nn.ReLU()
+
+        self.seq = torch.nn.Sequential(
+            torch.nn.Linear(in_features, 20_000),
+            torch.nn.ReLU(),
+            torch.nn.Linear(20_000, 10_000),
+            torch.nn.ReLU(),
+            torch.nn.Linear(10_000, 5_000),
+            torch.nn.ReLU(),
+            torch.nn.Linear(5_000, out_features)
+        )
 
     def forward(self, x):
-        x = self.linear1(x)
-        x = self.activation(x)
-        x = self.linear2(x)
-        x = self.activation(x)
-        x = self.linear3(x)
-        x = self.activation(x)
-        x = self.linear4(x)
-        x = self.activation(x)
-
+        x = self.seq(x)
         return x
 
 
-model = ImitationModel(
-    in_features=837, 
-    out_features=len(idx_to_move)
-).to(device)
+model = ImitationModel(in_features=837, out_features=len(idx_to_move)).to(device)
 
 
 # ----- Logging -----------------------------------
@@ -69,27 +91,63 @@ optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
 loss_fn = torch.nn.CrossEntropyLoss()
 
+scaler = torch.amp.GradScaler()
+
 for epoch in range(num_epochs):
 
+    # ---- TRAIN ----
     model.train()
-    total_loss = 0
+    train_loss = train_correct = train_seen = 0
 
     for batch_x, batch_y in train_loader:
-        batch_x = batch_x.to(device)
-        batch_y = batch_y.to(device)
+        batch_x, batch_y = batch_x.to(device, dtype=torch.float32), batch_y.to(device).long()
 
-        preds = model(batch_x)
-        loss = loss_fn(preds, batch_y)
+        with torch.amp.autocast(device_type=str(device)):
+            preds = model(batch_x)
+            loss  = loss_fn(preds, batch_y)
 
         optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
 
-        batch_loss = loss.item()
-        total_loss += batch_loss
+        train_loss += loss.item() * batch_y.size(0)
 
-    loss = total_loss / len(train_loader)
+        batch_correct = (preds.argmax(dim=1) == batch_y).sum().item()
+        batch_seen = batch_y.size(0)
 
-    run.log({"loss": loss})
+        train_correct += batch_correct
+        train_seen += batch_seen
+
+        run.log({"train/batch_loss": loss.item(), "train/batch_acc": batch_correct / batch_seen * 100.0})
+
+    epoch_train_loss = train_loss / train_seen
+    epoch_train_acc  = train_correct / train_seen * 100
+
+    # ---- VAL ----
+    model.eval()
+    val_loss = val_correct = val_seen = 0
+    with torch.no_grad(), torch.amp.autocast(device_type=str(device)):
+        for batch_x, batch_y in valid_loader:
+            batch_x, batch_y = batch_x.to(device, dtype=torch.float32), batch_y.to(device).long()
+            preds = model(batch_x)
+            loss  = loss_fn(preds, batch_y)
+
+            val_loss   += loss.item() * batch_y.size(0)
+            val_correct += (preds.argmax(1) == batch_y).sum().item()
+            val_seen   += batch_y.size(0)
+
+    epoch_val_loss = val_loss / val_seen
+    epoch_val_acc  = val_correct / val_seen * 100
+
+    run.log({
+        "epoch": epoch,
+        "train/loss": epoch_train_loss,
+        "train/acc":  epoch_train_acc,
+        "val/loss":   epoch_val_loss,
+        "val/acc":    epoch_val_acc,
+        "lr": optimizer.param_groups[0]["lr"],
+    })
+
 
 run.finish()
